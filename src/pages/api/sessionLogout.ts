7b5e1ad54e69a7a31fe7122d9e7b7851847ef496
Ivan <156574578+ivanlyubenov12@users.noreply.github.com>
2026-06-15 16:58:27 +0300

    Delete src directory

--- src/pages/api/sessionLogout.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== "POST") return res.status(405).end();
-
-    res.setHeader("Set-Cookie", "session=; Path=/; HttpOnly; Max-Age=0");
-    res.status(200).json({ status: "logged_out" });
-}
